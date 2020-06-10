57a6dcb043ae2fafc1cc3316d1b2856617173598
bkim <w1057216@g.ucla.edu>
2020-06-09 23:20:27 -0700

    Get existing DM chat or create new finished

--- server/main.go
+++ server/main.go
@@ -36,7 +36,7 @@ func main() {
 	// ls := "d76f73c3-0ea2-4afa-b009-794f1164910a"
 	// rh := "1e0c1b57-303a-407a-8c59-0b79e70c1ccc"
 
-	// models.LoadDirectMessageByUserUIDs(sz, rh)
+	// models.LoadDMDataByUserUIDs(sz, am)
 
 	http.ListenAndServe(":8080", r)
 
--- server/models/chat.go
+++ server/models/chat.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 // Chat type associates users and messages to target chat group
@@ -20,7 +22,7 @@ type Message struct {
 	FromUser User   `json:"user"`
 }
 
-func getDirectMessageChatByUserUIDs(aUID, bUID string) (Chat, error) {
+func getDMChatByUserUIDs(aUID, bUID string) Chat {
 	var uid, n string
 
 	s := `select 
@@ -53,18 +55,70 @@ func getDirectMessageChatByUserUIDs(aUID, bUID string) (Chat, error) {
 		log.Panic(err)
 	}
 
-	c := Chat{uid, n, ""}
+	c := Chat{}
+	if err != nil && err == sql.ErrNoRows {
+		c, _ = createNewDMChat(aUID, bUID)
+	} else {
+		c = Chat{uid, n, ""}
+	}
+
+	return c
+}
+
+func insertNewDMChat() Chat {
+	sql := `INSERT INTO chat
+				(uid)
+			VALUES
+				(?)`
+	stmt, _ := DB.Prepare(sql)
+	defer stmt.Close()
+
+	c := Chat{}
+	for {
+		uID, _ := uuid.NewV4()
+		_, err = stmt.Exec(uID.String())
+
+		if err == nil {
+			c.UID = uID.String()
+			break
+		}
+	}
+
+	return c
+}
+
+func insertNewDMChatUsers(c Chat, aUID, bUID string) error {
+	sql := `INSERT INTO chat_users
+				(chat_id, user_id)
+			VALUES
+				((select id from chat where uid = ?), (select id from users where uid = ?)),
+				((select id from chat where uid = ?), (select id from users where uid = ?))`
+
+	stmt, _ := DB.Prepare(sql)
+	defer stmt.Close()
+
+	_, err = stmt.Exec(c.UID, aUID, c.UID, bUID)
+
+	return err
+}
+
+func createNewDMChat(aUID, bUID string) (Chat, error) {
+
+	c := insertNewDMChat()
+
+	err := insertNewDMChatUsers(c, aUID, bUID)
+
+	if err != nil {
+		log.Panic(err)
+	}
 
 	return c, err
 }
 
-// LoadDirectMessageByUserUIDs handles...
-func LoadDirectMessageByUserUIDs(aUID, bUID string) {
-	c, err := getDirectMessageChatByUserUIDs(aUID, bUID)
-	if err != nil && err == sql.ErrNoRows {
-		// Create new chat and insert users in chat_users
-	} else {
-		fmt.Println(c)
-		// Load messages here
-	}
+// LoadDMDataByUserUIDs handles...
+func LoadDMDataByUserUIDs(aUID, bUID string) {
+
+	c := getDMChatByUserUIDs(aUID, bUID)
+
+	fmt.Println(c)
 }
