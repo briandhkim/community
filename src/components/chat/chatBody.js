6b5fc20a02048361ffedb4064162928aaf49ebed
bkim <w1057216@g.ucla.edu>
2020-06-27 17:48:54 -0700

    Restrict height for chat body and enable autoscroll to latest message

--- src/components/chat/chatBody.js
+++ src/components/chat/chatBody.js
@@ -7,7 +7,19 @@ const ChatBody = (props) => {
     useEffect(() => {
         const elem = document.querySelectorAll(".tooltipped");
         M.Tooltip.init(elem, {});
+
+        showLatestMessage();
     });
+
+    const showLatestMessage = () => {
+        const messages = document.querySelectorAll('.user-msg-container');
+
+        if (messages.length) {
+            const latestMessage = messages[messages.length-1];
+            
+            latestMessage.scrollIntoView();
+        }
+    };
 
     const noMessages = () => {
         return (
@@ -19,7 +31,7 @@ const ChatBody = (props) => {
 
     const messageLeft = (m, idx) => {
         return (
-            <div className="col s11 m9 l7 mb-1r" key={idx}>
+            <div className="col s11 m9 l7 mb-1r user-msg-container" key={idx}>
                 <div className="col s12 l10 mb-5">
                     <div className="p-10 inline-block min-w-100 tooltipped font-primary bg-primary-light text-secondary rounded-5 z-depth-2" data-position="bottom" data-tooltip={`${m.dateCreated}`}>
                         {m.text}
@@ -36,7 +48,7 @@ const ChatBody = (props) => {
 
     const messageRight = (m, idx) => {
         return (
-            <div className="col s11 offset-s1 m9 offset-m3 l7 offset-l5 mb-1r right-align" key={idx}>
+            <div className="col s11 offset-s1 m9 offset-m3 l7 offset-l5 mb-1r right-align user-msg-container" key={idx}>
                 <div className="col s12 l10 offset-l2 mb-5">
                     <div className="p-10 inline-block min-w-100 tooltipped font-primary left-align blue darken-2 white-text rounded-5 z-depth-2" data-position="bottom" data-tooltip={`${m.dateCreated}`}>
                         {m.text}
@@ -62,7 +74,7 @@ const ChatBody = (props) => {
     });
 
     return (
-        <div className="chat-body row blue-grey lighten-5 p-105r mb-0 min-h-500">
+        <div className="chat-body row blue-grey lighten-5 p-105r mb-0 min-h-500 overflow-y-auto custom-scroll-bar max-h-550">
             {messages.length ? renderMessages : noMessages()}
         </div>
     );
